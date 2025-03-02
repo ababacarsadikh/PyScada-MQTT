6931b2264cce3c2d253fda6970d45c27375dcf01
ababacarsadikh <48486198+ababacarsadikh@users.noreply.github.com>
2025-03-02 15:12:26 +0100

    Update device.py

--- pyscada/mqtt/device.py
+++ pyscada/mqtt/device.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import paho.mqtt.client as mqtt
-from pyscada.device import GenericDevice
-from .devices import GenericDevice as GenericHandlerDevice
-
-
 try:
     import paho.mqtt.client as mqtt_client
 
@@ -13,56 +8,113 @@ try:
 except ImportError:
     driver_ok = False
 
+from math import isnan, isinf
 from time import time
+from datetime import datetime
+
+import traceback
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
 
-class Device(GenericDevice):
+def _default_decoder(value):
+    return value[0]
+
+
+class Device:
+    """
+    MQTT Broker (device) class
+    """
+
     def __init__(self, device):
-        self.driver_ok = driver_ok
-        self.handler_class = GenericHandlerDevice
-        super().__init__(device)
+        self.device = device
+        self._address = device.mqttbroker.address
+        self._port = device.mqttbroker.port
+        self._timeout = device.mqttbroker.timeout
+        self._device_not_accessible = 0
+        self.variables = {}
+        self.data = (
+            {}
+        )  # holds the raw data for each topic, Value is None if no new data is there
+        self.broker = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2)
+        self.broker.on_connect = self.on_connect
+        self.broker.on_message = self.on_message
+        self.broker.username = device.mqttbroker.username
+        self.broker.password = device.mqttbroker.password
+        self._connect()
 
-        for var in self.device.variable_set.filter(active=1):
-            if not hasattr(var, "mqttvariable"):
-                continue
-            self.variables[var.pk] = var
-
-        if self.driver_ok and self.driver_handler_ok:
-            self._h.connect()
-        else:
-            logger.warning(f"Cannot import mqtt or handler for {self.device}")
-
-    def write_data(self, topic, value, task):
+    def _connect(self):
         """
-        write value to the instrument/device
+        connect to the MQTT Broker
         """
-        output = []
-        if not self.driver_ok:
-            logger.info("Cannot import MQTT client")
-            return output
+        status = self.broker.connect(self._address, int(self._port), int(self._timeout))
+        self.broker.loop_start()  # start the comunication thread
+        return status
 
-        for item in self.variables.values():
-            if item.id == variable_id):
-                continue
-            
-            read_value = self._h.write_data(variable_id, value, task)
-            if read_value is not None and item.update_values([read_value], [time()]):
-                output.append(item)
-            else:
-                logger.debug(f"No data to read after write for {task}")
-        return output
+    def _disconnect(self):
+        """
+        close the connection to the MQTT Broker
+        """
+        self.broker.loop_stop()
 
     def request_data(self):
-        """
-        request data from the instrument/device
-        """
+        """process the data that was recived from the broker since last call"""
         output = []
-        if not self.driver_ok:
-            logger.info("Cannot import paho mqtt")
-            return output
+        keys_to_reset = []
+        for variable_id, variable in self.variables.items():
+            if self.data[variable.mqttvariable.topic] is not None:
+                value = self.data[variable.mqttvariable.topic].decode("utf-8")
+                value = variable.mqttvariable.parse_value(value)
+                timestamp = time()
 
-        output = super().request_data()
+                if variable.mqttvariable.timestamp_topic is not None:
+                    if self.data[variable.mqttvariable.timestamp_topic] is None:
+                        logger.debug("mqtt request_data timestamp_topic is None")
+                        continue
+
+                    timestamp = self.data[variable.mqttvariable.timestamp_topic].decode(
+                        "utf-8"
+                    )
+                    timestamp = variable.mqttvariable.parse_timestamp(
+                        timestamp
+                    )  # convert
+
+                    keys_to_reset.append(variable.mqttvariable.timestamp_topic)
+
+                self.data[variable.mqttvariable.topic] = (
+                    None  # reset value for next loop, this is done here for the case that we recieved the value, but waiting for the timestamp
+                )
+
+                if variable.update_values([value], [timestamp]):
+                    output.append(variable)
+        for key in keys_to_reset:
+            self.data[key] = None  # reset value for next loop
         return output
+
+    def on_connect(self, client, userdata, flags, reason_code, properties):
+        """will be called if the client connects to the broker"""
+        # print(f"Connected with result code {reason_code}") FIXME add logging
+        logger.debug(f"mqtt on_connect {reason_code}")
+        try:
+            for variable in self.device.variable_set.filter(active=1):
+                if not hasattr(variable, "mqttvariable"):
+                    continue
+                self.variables[variable.pk] = variable
+                client.subscribe(variable.mqttvariable.topic)  # value Topic
+                self.data[variable.mqttvariable.topic] = None
+                if variable.mqttvariable.timestamp_topic is not None:
+                    client.subscribe(
+                        variable.mqttvariable.timestamp_topic
+                    )  # timestamp Topic
+                    self.data[variable.mqttvariable.timestamp_topic] = None
+        except:
+            logger.warning(traceback.format_exc())
+
+    def on_message(self, client, userdata, msg):
+        """callback for new PUBLISH messages, is called on receive from Server"""
+        logger.debug(msg.topic + " " + str(msg.payload))
+        if msg.topic not in self.data:
+            return
+        self.data[msg.topic] = msg.payload
